3dbb7a1a19399797785549d796bd71b87a9fa774
Ali Mirzaei Majd <pe4090731@gmail.com>
2023-06-15 16:17:08 -0700

    all events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { getAllEvents } from "@/dummy-data";
+import Eventlist from "@/components/events/event-list";
 
 const events = () => {
-  return <div>events</div>;
+  const events = getAllEvents();
+
+  return <Eventlist items={events} />;
 };
 
 export default events;
